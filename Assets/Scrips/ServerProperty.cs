150ba4d4f67472b91be5c5612c372fa22dce317d
HappyPiggy <296056428@qq.com>
2016-11-04 22:58:47 +0800

    add serverpanel

---
+++ Assets/Scrips/ServerProperty.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerProperty : MonoBehaviour
+{
+
+    public string ip   = "127.0.0.1:8080";
+
+    private string _name;
+   // private bool isPress = false;
+
+    public string name
+    {
+        set
+        {
+            //value为当前的name值  在别处实例化时候赋值
+            transform.FindChild("Label").GetComponent<UILabel>().text = value;
+            _name = value;
+        }
+        get { return _name; }
+    }
+    public int count   = 100;
+
+
+    public void OnPress(bool isPress)
+    {
+        if (!isPress)
+        {
+            transform.root.SendMessage("OnServerSelected",gameObject);
+        }
+    }
+
+
+}
--- Assets/Scrips/StartController.cs
+++ Assets/Scrips/StartController.cs
@@ -1,22 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SwitchController : MonoBehaviour
+public class StartController : MonoBehaviour
 {
 
 
     public TweenScale startPanel;
     public TweenScale loginPanel;
     public TweenScale registerPanel;
+    public TweenScale serverPanel;
+
 
     public UIInput UserNameInputLogin;
     public UIInput UserPwdInputLogin;
     public UILabel UserNameLabelStart;
+    public UILabel ServerLabelStart;
+
+    public UIGrid serverListGrid;
+    public GameObject redServer;
+    public GameObject greenServer;
+    public GameObject currentServer;
 
     private  float waitSec =0.4f;
+
     private static string userName;
     private static string userPwd;
+    private static ServerProperty sp;
 
+
+    void Start()
+    {
+        InitServerList();
+    }
+
+
+    void InitServerList()
+    {
+        //TODO
+        //读取游戏服务器个数 动态加载服务器列表
+
+
+        //模拟  假设有20个服务器
+        for (int i = 0; i < 20; i++)
+        {
+            //模拟随机生成每个区信息
+            string ip = "127.0.0.1:8080";
+            string name = i + 1 + "区 南三食堂";
+            int count = Random.Range(0, 100);
+
+
+            //每个go都是代表一个服务器
+            GameObject go = null;
+
+            if (count > 50)
+            {
+                //火爆
+                go = NGUITools.AddChild(serverListGrid.gameObject, redServer);
+            }
+            else
+            {
+                //流畅
+                go = NGUITools.AddChild(serverListGrid.gameObject, greenServer);
+            }
+
+            ServerProperty sp = go.GetComponent<ServerProperty>();
+            sp.ip = ip;
+            sp.name = name;
+            sp.count = count;
+
+            serverListGrid.AddChild(go.transform);
+        }
+
+
+        
+    }
+
+    
+    public void OnServerSelected(GameObject go)
+    {
+        sp = go.GetComponent<ServerProperty>();
+
+        //修改当前服务器的 sprite 和name为 选中服务器
+        currentServer.GetComponent<UISprite>().spriteName = go.GetComponent<UISprite>().spriteName;
+        currentServer.transform.FindChild("Label").GetComponent<UILabel>().text = sp.name;
+    }
+
+    public void OnCurrentServerBtnClick()
+    {
+        //切换为start界面
+        serverPanel.PlayReverse();
+        StartCoroutine(HiderObj(serverPanel.gameObject));
+
+        startPanel.gameObject.SetActive(true);
+        startPanel.PlayReverse();
+
+
+        //服务器label更新
+        ServerLabelStart.text = sp.name;
+    }
 
     public void OnLoginBtnClick()
     {
@@ -102,7 +183,11 @@ public class SwitchController : MonoBehaviour
 
     public void OnServceNameClick()
     {
-        
+        startPanel.PlayForward();
+        StartCoroutine(HiderObj(startPanel.gameObject));
+
+        serverPanel.gameObject.SetActive(true);
+        serverPanel.PlayForward();
     }
 
 
