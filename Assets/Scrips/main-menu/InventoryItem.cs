5452159ff1cf98084a789bc9177fa1d5a35ada11
HappyPiggy <296056428@qq.com>
2016-11-27 23:50:01 +0800

    add INIT knapsack

---
+++ Assets/Scrips/main-menu/InventoryItem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryItem
+{
+
+    private Inventory inventory;
+    private int level;
+    private int count;
+
+    public int Level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+
+    public int Count
+    {
+        set { count = value; }
+        get { return count; }
+    }
+
+    public Inventory Inventory
+    {
+        set { inventory = value; }
+        get { return inventory; }
+    }
+
+}
